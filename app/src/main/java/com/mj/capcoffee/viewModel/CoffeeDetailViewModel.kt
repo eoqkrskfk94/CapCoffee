18441ecff6a2475320e5e1bc742b45727dc4dba7
김명진 <mobinity.kmj@gmail.com>
2020-12-13 18:06:24 +0900

    [Add] 네이버 뷰, 최저가 버튼 추가

--- app/src/main/java/com/mj/capcoffee/viewModel/CoffeeDetailViewModel.kt
+++ app/src/main/java/com/mj/capcoffee/viewModel/CoffeeDetailViewModel.kt
@@ -1,23 +1,65 @@
 package com.mj.capcoffee.viewModel
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.view.View
-import com.mj.capcoffee.WebViewActivity
+import com.mj.capcoffee.R
 
 class CoffeeDetailViewModel {
 
     fun clickNaverViewBtn(coffee : String, brand : String, view : View){
-        val intent = Intent(view.context, WebViewActivity::class.java)
-        intent.putExtra("coffee", coffee)
-        intent.putExtra("brand", brand)
+
+        var coffeeName = coffee
+        var brandName = brand
+
+        if(brandName == (view.context as Activity).getString(R.string.starbucks)) brandName = (view.context as Activity).getString(R.string.starbucks_kr)
+        else if(brandName == (view.context as Activity).getString(R.string.illy)) brandName = (view.context as Activity).getString(R.string.illy_kr)
+        else if(brandName == (view.context as Activity).getString(R.string.nespresso)) brandName = (view.context as Activity).getString(R.string.nespresso_kr)
+        else if(brandName == (view.context as Activity).getString(R.string.dunkin)) brandName = (view.context as Activity).getString(R.string.dunkin_kr)
+        else if(brandName == (view.context as Activity).getString(R.string.paulBassett)) brandName = (view.context as Activity).getString(R.string.paulBassett_kr)
+
+
+        coffeeName = "$brandName $coffeeName 캡슐"
+        coffeeName = coffeeName.replace("\\s".toRegex(), "+")
+
+        println(coffeeName)
+
+
+        val intent =  Intent(Intent.ACTION_VIEW, Uri.parse("https://search.naver.com/search.naver?where=view&sm=tab_jum&query=$coffeeName"))
         view.context.startActivity(intent)
+
+
     }
 
     fun clickNaverPriceBtn(coffee : String,brand : String, view : View){
-        val intent = Intent(view.context, WebViewActivity::class.java)
-        intent.putExtra("coffee", coffee)
-        intent.putExtra("brand", brand)
+
+        var coffeeName = coffee
+        var brandName = brand
+
+
+        if(brandName == (view.context as Activity).getString(R.string.starbucks)) brandName = (view.context as Activity).getString(R.string.starbucks_kr)
+        else if(brandName == (view.context as Activity).getString(R.string.illy)) brandName = (view.context as Activity).getString(R.string.illy_kr)
+        else if(brandName == (view.context as Activity).getString(R.string.nespresso)) brandName = (view.context as Activity).getString(R.string.nespresso_kr)
+        else if(brandName == (view.context as Activity).getString(R.string.dunkin)) brandName = (view.context as Activity).getString(R.string.dunkin_kr)
+        else if(brandName == (view.context as Activity).getString(R.string.paulBassett)) brandName = (view.context as Activity).getString(R.string.paulBassett_kr)
+
+
+        coffeeName = "$brandName $coffeeName 캡슐"
+        coffeeName = coffeeName.replace("\\s".toRegex(), "+")
+
+        println(coffeeName)
+
+
+        val intent =  Intent(Intent.ACTION_VIEW, Uri.parse("https://msearch.shopping.naver.com/search/all?query=$coffeeName"))
         view.context.startActivity(intent)
+
+
+    }
+
+    fun clickBackBtn(view : View){
+        (view.context as Activity).finish()
+
     }
 
 
