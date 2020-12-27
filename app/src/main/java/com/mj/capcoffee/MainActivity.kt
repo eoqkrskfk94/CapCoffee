7364e9fb837bf20aaaba95c3532643d3f5abf30f
김명진 <mobinity.kmj@gmail.com>
2020-12-27 15:26:53 +0900

    [Add] 파이어베이스 analytics, crashlytics, admob 연동

--- app/src/main/java/com/mj/capcoffee/MainActivity.kt
+++ app/src/main/java/com/mj/capcoffee/MainActivity.kt
@@ -5,15 +5,23 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import com.mj.capcoffee.databinding.ActivityMainBinding
 import com.mj.capcoffee.viewModel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.viewModel = MainViewModel()
+
+        firebaseAnalytics = Firebase.analytics
+
 
 
         MobileAds.initialize(this) {}
